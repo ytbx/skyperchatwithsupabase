a3a43714adbbf20ea2ff09cacdb9e2945b144966
ytbx <yusuftalhabarlas@gmail.com>
2025-12-01 16:55:13 +0300

    Fix build error by removing source-identifier plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import sourceIdentifierPlugin from 'vite-plugin-source-identifier'
 
 const isProd = process.env.BUILD_MODE === 'prod'
 export default defineConfig({
   base: './',
   plugins: [
-    react(),
-    sourceIdentifierPlugin({
-      enabled: !isProd,
-      attributePrefix: 'data-matrix',
-      includeProps: true,
-    })
+    react()
   ],
   define: {
     global: 'globalThis',
